0d073324dffe48e31de81d3fce448718bb4fb79b
cristina <cris78seijas@gmail.com>
2022-08-22 20:50:00 -0300

    corrigiendo errores

--- src/main/java/com/portfolio/spring/Application.java
+++ src/main/java/com/portfolio/spring/Application.java
@@ -17,7 +17,7 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("https://cvsfrontend-38ffe.web.app").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
