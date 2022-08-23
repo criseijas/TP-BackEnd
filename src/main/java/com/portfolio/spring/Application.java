768c16927393cfeb84e59bc3693d510c91f9ae90
cristina <cris78seijas@gmail.com>
2022-08-23 00:40:19 -0300

    make it better

--- src/main/java/com/portfolio/spring/Application.java
+++ src/main/java/com/portfolio/spring/Application.java
@@ -2,9 +2,9 @@ package com.portfolio.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -12,14 +12,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-@Bean
+/*@Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("**").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
             }
         };
-    }
+    }*/
 
 }
